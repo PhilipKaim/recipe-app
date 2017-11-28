bad413385393e165ced79acd5b70a7671890f78c
Chillup <philipkaim@gmail.com>
2017-11-28 18:01:47 -0500

    edit expense page functional

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,16 +1,11 @@
 import React from 'react';
-import { removeExpense } from '../actions/expenses';
-import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-const ExpenseListItem = ({ description, amount, createdAt, id, dispatch }) => (
+const ExpenseListItem = ({ description, amount, createdAt, id }) => (
   <div>
-    <h3>{description}</h3>
+    <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
     <p>{amount} - {createdAt}</p>
-    <button onClick={() => {
-      dispatch(removeExpense({ id }));
-    }}>Remove</button>
-
   </div>
 );
 
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
